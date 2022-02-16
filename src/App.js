5cfbc00a8cb236cbbf631c304a8358c34dceb653
Abdullahi M. Ibrahim <abdullah98@hotmail.se>
2022-02-16 16:05:42 +0100

    Förmiddag föreläsning 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
+import Todo from './Todo';
 function App() {
+  const [id, setId] = useState(0)
+  const incrementCounter = () => {
+    setId((previousValue) => previousValue + 1)
+  }
+  const [title, setTitle] = useState('')
+  const [todos, setTodos] = useState([])
+  const addTodo = () => {
+    const newTodo = {
+      id: id,
+      title, 
+      description: 'Ha en bra dag!'
+    }
+    setTodos([...todos, newTodo])
+    incrementCounter()
+  }
+  const deleteTodo = (id) => {
+    const filteredTodos = todos.filter((todos) => todos.id !==id)
+    setTodos(filteredTodos)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{display: 'flex'}}>
+        <label><input value={title} onChange={(event) => {
+          console.log(event.target.value)
+          setTitle(event.target.value)
+        }} /></label>
+        <label><input onChange={() => {
+
+        }} /></label>
+        <button onClick={addTodo}>ADD TODO</button>
+    </div>
+      {todos.map(({id, titel, description}) => {
+      return <Todo key={id} id = {id} title={title} description = {description} onDelete = {deleteTodo} />
+      })}
     </div>
   );
 }
