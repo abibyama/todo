3d0522440995a94b4e05a4c0be9c68289ebcc744
Abdullahi M. Ibrahim <abdullah98@hotmail.se>
2022-02-17 15:50:34 +0100

    Uppdatering med bättre namngivning av ID

--- src/App.js
+++ src/App.js
@@ -1,40 +1,43 @@
 import { useState } from 'react';
 import './App.css';
 import Todo from './Todo';
+
 function App() {
   const [id, setId] = useState(0)
   const incrementCounter = () => {
     setId((previousValue) => previousValue + 1)
   }
+
   const [title, setTitle] = useState('')
   const [todos, setTodos] = useState([])
   const addTodo = () => {
+    console.log(title)
     const newTodo = {
-      id: id,
-      title, 
+      todoId: id,
+      title: title,
       description: 'Ha en bra dag!'
     }
     setTodos([...todos, newTodo])
     incrementCounter()
   }
-  const deleteTodo = (id) => {
-    const filteredTodos = todos.filter((todos) => todos.id !==id)
+  const deleteTodo = (deleteId) => {
+    const filteredTodos = todos.filter((todo) => todo.todoId !== deleteId)
     setTodos(filteredTodos)
   }
+
   return (
     <div>
       <div style={{display: 'flex'}}>
-        <label><input value={title} onChange={(event) => {
-          console.log(event.target.value)
+        <label>Title<input value={title} onChange={(event) => {
           setTitle(event.target.value)
         }} /></label>
-        <label><input onChange={() => {
-
+        <label>Description<input onChange={() => {
+          //Vad kan vi göra för att koppla denna input till description? :)
         }} /></label>
         <button onClick={addTodo}>ADD TODO</button>
-    </div>
-      {todos.map(({id, titel, description}) => {
-      return <Todo key={id} id = {id} title={title} description = {description} onDelete = {deleteTodo} />
+      </div>
+      {todos.map(({todoId, title, description}) => {
+        return <Todo key={todoId} id={todoId} title={title} description={description} onDelete={deleteTodo} />
       })}
     </div>
   );
